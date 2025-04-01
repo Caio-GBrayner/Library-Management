6f2fd0d8dcf20fa2f4a6794f0457ed1f3438816a
Caio-GBrayner <caiogomesbrayner@gmail.com>
2025-04-01 19:07:44 -0300

    order model

---
+++ backend/app/models/order.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from ..extensions import db
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, Enum
+from sqlalchemy.orm import relationship, backref
+
+class Order(db.Model):
+    __tablename__ = "TB_ORDER"
+
+    id = db.Column(db.Integer, primary_key=True)
+    moment = db.Column(db.DateTime, nullabel=False, default=datetime.utcnow)
+    client_id = db.Column(db.Integer, db.ForeignKey('tb_user.id'))
+    client = db.relationship("User", backref=backref("orders", lazy=True))
+    items = db.relationship("OrderItem", backref="order", lazy=True, cascade="all, delete-orphan")
+    order_status = db.Column(db.Integer, nullable=False)
+    payment = db.relationship("Payment", uselist=False, back_populates="order", cascade="all, delete-orphan")
